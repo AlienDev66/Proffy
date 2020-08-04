9137e7086f35dab2bb6e0284c7b4fb1a8fb491af
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-08-04 19:37:25 +0100

    :sparkles: Configurando o servidor, configurando a database e criando as tabelas.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,31 @@
-import express from "express";
+import express, { response } from "express";
 
-const app = express();
+const app = express();~
+
+app.use(express.json());
+
+app.post('/users', (request, response)=>{
+
+    const Users = [
+    {
+        nome: 'Domingos Capitango',
+        idade: 19,
+        genero: "Masculino"
+    },
+    {
+        nome: 'Domingos Capitango',
+        idade: 19,
+        genero: "Masculino"
+    },
+    {
+        nome: 'Domingos Capitango',
+        idade: 19,
+        genero: "Masculino"
+    },
+]
+
+return response.json(Users);
+
+})
 
 app.listen(3333);
